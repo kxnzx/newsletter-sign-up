de42d0082bff575022ac30580fef84e50f2af40d
“kxnzx” <k.bousboa@gmail.com>
2023-10-01 14:51:50 +0200

    edit

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+Your users should be able to:
+
+- Add their email and submit the form
+- See a success message with their email after successfully submitting the form
+- See form validation messages if:
+  - The field is left empty
+  - The email address is not formatted correctly
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+*/
+
+"use strict";
+
+// Select all the buttons:
+const buttons = document.querySelectorAll("buttons");
+
+// Select Succes Message:
+const message = document.getElementsByClassName("succes_message");
+
+// Add listeners to all of them:
+buttons.forEach((button) => {
+  button.addEventListener("click", toggle);
+});
+
+// When button is clicked toggle display
+function toggle() {
+  if (message.display.style === "none") {
+    message.display.style === "block";
+  } else {
+    message.display.style === "none";
+  }
+}
+
+toggle();
